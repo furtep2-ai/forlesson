892ceb3ad439e6750fc9ec527c18e5c0768994e8
furte <furtep2@gmail.com>
2026-03-29 18:14:01 +0600

    first commit

---
+++ test_create_manage.py
@@ -0,0 +1,55 @@
+import unittest
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+    host='localhost',
+    user='user',
+    password='12345',
+    database='cursovoi'
+)
+
+def search(tovar_id=None,name=None,price=None):
+    with get_connection() as conn:
+        cursor=conn.cursor()
+        if tovar_id is not None and price is None and name is None:
+            try:
+                tovar_id=int(tovar_id)
+            except ValueError as e:
+                print("Ошибка в товар айди")
+            cursor.execute("select * from products where id=%s",(tovar_id,))
+            tovar=cursor.fetchone()
+            if tovar:
+                return True
+            else:
+                return False
+        elif name is not None and price is not None and tovar_id is None:
+            cursor.execute("select * from products where name=%s and price=%s",(name,price))
+            tovar=cursor.fetchone()
+            if tovar:
+                return True
+            else:
+                return False
+        elif name is not None and price is not None and tovar_id is not None:
+            cursor.execute("select * from products where id=%s and name=%s and price=%s",(tovar_id,name,price))
+            tovar=cursor.fetchone()
+            if tovar:
+                return True
+            else:
+                return False
+        
+        
+class Testsearch(unittest.TestCase):
+    def test_check_search1(self):
+        self.assertTrue(search(name="ret",price=435))
+    def test_check_search2(self):
+        self.assertFalse(search(name="try",price=232))
+    def test_check_search3(self):
+        self.assertTrue(search(tovar_id=1))
+    def test_check_search4(self):
+        self.assertTrue(search(tovar_id=2,name="ret",price=435))
+    def test_check_search5(self):
+        self.assertFalse(search(tovar_id=3,name="ret",price=435))
+    
+if __name__=="__main__":
+    unittest.main()
---
+++ test_register_login.py
@@ -0,0 +1,65 @@
+import mysql.connector
+import re
+import unittest
+def get_connection():
+    return mysql.connector.connect(
+    host='localhost',
+    user='user',
+    password='12345',
+    database='cursovoi'
+)
+pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$"
+
+def register(name,password):
+        if re.match(pattern,password):
+            if re.match(pattern,name):
+                with get_connection() as conn:
+                    cursor=conn.cursor()
+                    cursor.execute("insert into users1 (name,password) values (%s,%s)",(name,password))
+                    conn.commit()
+                    cursor.execute("select * from users1 where name=%s and password=%s",(name,password))
+                    check=cursor.fetchone()
+                    if check:
+                        print("Поздравляю вы успешно зарегистрировались")
+                        return True
+                    else:
+                        print("Что то пошло не так")
+                        return False
+        else:
+            return False
+def login(name,password):
+        with get_connection() as conn:
+            cursor=conn.cursor()
+            cursor.execute("select * from users1 where name=%s and password=%s",(name,password))
+            user=cursor.fetchone()
+            if user:
+                print("Хорошо вы вошли в систему")
+                return True
+            else:
+                print("Такого пользователя не существует")
+                return False
+
+class Testreg(unittest.TestCase):
+    def test_register1(self):
+        self.assertTrue(register("Namee24235","35352Fssg"))
+
+    def test_register2(self):
+        self.assertFalse(register("Namee24235","35352Fssg+_- "))
+
+    def test_register3(self):
+        self.assertFalse(register("Namee24235+=2--=-=--","35352Fssg"))
+
+    def test_login1(self):
+        self.assertTrue(login("ret","Gersolo2009"))
+
+    def test_login2(self):
+        self.assertFalse(login("ret ","Gersolo2009"))
+
+    def test_login3(self):
+        self.assertFalse(login("ret","Gersolo2009 "))
+
+    def test_login4(self):
+        self.assertFalse(login("ret+","Gersolo200923"))
+        
+if __name__=="__main__":
+    unittest.main()
